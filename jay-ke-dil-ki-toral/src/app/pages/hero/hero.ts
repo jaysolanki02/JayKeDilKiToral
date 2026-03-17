f20f1d43c08f84c9132f47560bc676cdef0c8655
Jay Solanki <jayso@nedbank.co.za>
2026-03-17 18:19:31 +0530

    scroll issue fix

--- jay-ke-dil-ki-toral/src/app/pages/hero/hero.ts
+++ jay-ke-dil-ki-toral/src/app/pages/hero/hero.ts
@@ -28,7 +28,6 @@ export class Hero implements AfterViewInit, OnInit, OnDestroy {
     'images/personal/traditional.jpeg',
     'images/personal/t-j-3.jpeg',
     'images/personal/t-j-2.jpeg',
-    'images/personal/t-j-5.jpeg',
     'images/personal/tdp.jpeg',
     'images/personal/11-11.jpeg',
   ];
@@ -54,8 +53,8 @@ export class Hero implements AfterViewInit, OnInit, OnDestroy {
     setTimeout(() => {
       this.updateHeight(); 
     });
-    this.setHeartbeatToFonts();
     this.startSlideshow();
+    // this.setHeartbeatToFonts();
   }
 
   ngOnDestroy() {
@@ -71,21 +70,21 @@ export class Hero implements AfterViewInit, OnInit, OnDestroy {
   updateHeight() {
 
     this.hero.nativeElement.style.height = this.hero.nativeElement.offsetWidth + 'px';
-    this.handsTogether.nativeElement.style.height = this.handsTogether.nativeElement.offsetWidth/1.5 + 'px';
+    // this.handsTogether.nativeElement.style.height = this.handsTogether.nativeElement.offsetWidth/1.5 + 'px';
     this.meetTheBride.nativeElement.style.height = this.meetTheBride.nativeElement.offsetWidth/1.4 + 'px';
     this.meetTheGroom.nativeElement.style.height = this.meetTheGroom.nativeElement.offsetWidth/1.4 + 'px';
     
     this.content.nativeElement.style.minWidth = (this.hero.nativeElement.offsetWidth -1) + 'px';
     const rect = this.content.nativeElement.getBoundingClientRect();
 
-    this.isparrallaxVisible = rect.top < window.innerHeight && rect.bottom >= 200;
+    this.isparrallaxVisible = rect.top < window.innerHeight && rect.bottom >= 300;
     if (this.isparrallaxVisible) {
       this.setbgPositionAndScroll();
     }
   }
 
   setHeartbeatToFonts() {
-    this.wedTitleInterval = setInterval(() => {
+    // this.wedTitleInterval = setInterval(() => {
     const wedTitle = document.querySelector('.wed-title') as HTMLElement;
     let margin = window.innerHeight;
     let factor = window.innerHeight;
@@ -96,21 +95,25 @@ export class Hero implements AfterViewInit, OnInit, OnDestroy {
       margin = window.innerHeight;
       factor = 0.7;
     } else {
-      factor = window.innerHeight / window.innerWidth;
+      factor = window.innerHeight / window.innerWidth ;
       margin = window.innerHeight;
     }
-      wedTitle.style.minHeight = factor * margin + 'px' ;
+    wedTitle.style.minHeight = factor * margin + 'px' ;
       // if(wedTitle.style.fontSize === '5rem') {
       //   wedTitle.style.fontSize = '6rem';
       // } else {
       //   wedTitle.style.fontSize = '5rem'
       // }
-    }, 1160);
+    // }, 1160);
   }
 
   setbgPositionAndScroll() {
-    const scroll = window.scrollY;
-    const factorScx = window.innerWidth < 768 ? 0.75 : 0.25;
+    const factorScx = window.innerWidth < 768 ? 0.75 : 0.3;
+    let y =  0;
+    if (window.scrollY > 300) {
+      // subtract start point so it starts from 0 smoothly
+      y = (window.scrollY - 300) * factorScx;
+    }
     [
       document.querySelector('.root-landing-img') as HTMLElement,
       document.querySelector('.hands-together-img') as HTMLElement,
@@ -119,7 +122,7 @@ export class Hero implements AfterViewInit, OnInit, OnDestroy {
       document.querySelector('.memories-container') as HTMLElement,
       document.querySelector('.events-container') as HTMLElement,
     ].forEach(bg=> {
-      bg.style.transform = `translateY(${Math.floor(scroll * factorScx)}px)`;
+      bg.style.transform = `translateY(${y}px)`;
     })
     setTimeout(() => {
       const gtlPO = document.querySelector('.gtl-po') as HTMLElement;
